edcef3ae1741215390b23710a52e2392a4b67bab
Palak Patel <Papatel@ainq.com>
2023-05-04 23:11:29 -0400

    fix: Add additional condition to improve auditing performance

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { audit_history_changeType } from "@prisma/client";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+import fetch from "node-fetch";
 
 const showSql =
   process.env.SHOW_SQL_IN_CONSOLE?.toLocaleLowerCase() === "true" || false;
@@ -8,51 +11,83 @@ const prisma = new PrismaClient({
   log: showSql ? ["query", "info", "warn", "error"] : [],
 });
 
-prisma.$use(async (params, next) => {
-  
-  let changeType;
-  let oldValues={};
-  let newValues={};
-  //It is good to have prams.model here to have better performance
-  if (params.model === "jurisdiction" && params.action === "create") {
-    newValues= params.args.data
-    changeType= audit_history_changeType.Insert;
+const auditMiddleware = async (params, next) => {
+  // const session = await getSession();
+  // const userName = session?.user?.name ?? 'anonymous';
 
-  } 
-  else if (params.model === "destinations" && params.action === "update") {
-    oldValues = await prisma[params.model.toString()].findUnique({
-                    where: { dest_id: params.args.where.dest_id },
-                });
-    newValues= params.args.data
-    changeType= audit_history_changeType.Update;
-  } 
-  else if (params.action === 'delete') {
-    oldValues = await prisma[params.model.toString()].findUnique({ where: { dest_id: params.args.where.dest_id } });
-    changeType = audit_history_changeType.Delete;
+  // // Ignore audit log for authentication actions
+  // if (params.action !== ) {
+  //   return next(params);
+  // }
+  const response = await fetch("http://localhost:3000/api/auth/session");
+  const statusResponse = await response.json();
+  console.log(response.status);
+
+  if (
+    !["create", "update", "delete"].includes(params.action) ||
+    params.model === "audit_history"
+  ) {
+    return next(params);
   }
 
-  const auditTrailData = {
-    tableName: params.model,
-    userName: "user",
-    changeType,
-    oldValues,
-    newValues,
-    createdAt: new Date(),
-  };
+  if (params.action === "create") {
+    const auditTrailData = {
+      tableName: params.model,
+      userName: "userName",
+      changeType: audit_history_changeType.Insert,
+      oldValues: {},
+      newValues: params.args.data,
+      createdAt: new Date(),
+    };
 
-   prisma.audit_history.create({ data: auditTrailData });
-  return await next(params);
-});
+    await prisma.audit_history.create({ data: auditTrailData });
+  }
+  if (params.action === "update") {
+    const record = await prisma[params.model.toString()].findUnique({
+      where: { dest_id: params.args.where.dest_id },
+    });
 
-// prisma.$on("query", (e) => {
-//   console.log("Query: " + e.query);
-//   console.log("Params: " + e.params);
-//   console.log("Duration: " + e.duration + "ms");
-// });
+    const auditTrailData = {
+      tableName: params.model,
+      userName: "userName",
+      changeType: audit_history_changeType.Update,
+      oldValues: record || { error: "Undefined" },
+      newValues: params.args.data,
+      createdAt: new Date(),
+    };
+
+    await prisma.audit_history.create({ data: auditTrailData });
+  } else if (params.action === "delete") {
+    const record = await prisma[params.model.toString()].findUnique({
+      where: { dest_id: params.args.where.dest_id },
+    });
+    const auditTrailData = {
+      tableName: params.model,
+      userName: "userName",
+      changeType: audit_history_changeType.Delete,
+      oldValues: record,
+      newValues: params.args.data,
+      createdAt: new Date(),
+    };
+    await prisma.audit_history.create({ data: auditTrailData });
+  }
+  const result = await next(params);
+  return result;
+};
+
+prisma.$use(auditMiddleware);
+
 export interface Context {
   prisma: PrismaClient;
+  session: {
+    user: {
+      name: string;
+      email: string;
+      image: string;
+    };
+  };
 }
 
-export const context: Context = {
+export const context = {
   prisma: prisma,
 };
